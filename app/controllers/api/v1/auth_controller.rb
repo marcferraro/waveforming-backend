7172ffdaaf2908e8b0761deaae4d77001542cf58
Marc <marc.ferraro1@gmail.com>
2021-03-23 12:15:08 -0500

    serializer and seed

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,13 +1,17 @@
 class Api::V1::AuthController < ApplicationController
 
     def create
-        byebug
+
         user = User.find_by(username: params[:username])
         
         if user
-
+            if user.authenticate(params[:password])
+                render json: user
+            else
+                render json: {error: "Wrong password."}
+            end
         else
-            render json: {error: "Invalid username or password."}
+            render json: {error: "User not found."}
         end
 
     end
