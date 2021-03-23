fd012e2ec5ef517cb7c3e93e0bf3c6d9fb14d711
Marc <marc.ferraro1@gmail.com>
2021-03-23 12:06:03 -0500

    password digest and has_secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2021_03_23_163113) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
