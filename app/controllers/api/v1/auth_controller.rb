ea8ecba1a1232f79c11f4935dfef3c0b60da67b6
Marc <marc.ferraro1@gmail.com>
2021-03-23 11:56:11 -0500

    bcrypt and error message

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,7 +2,14 @@ class Api::V1::AuthController < ApplicationController
 
     def create
         byebug
-        user = User.find_by(username: params[:username])    
+        user = User.find_by(username: params[:username])
+        
+        if user
+
+        else
+            render json: {error: "Invalid username or password."}
+        end
+
     end
     
 end
