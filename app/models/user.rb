32e986b992041845e7ea4f7958a220c6cf92fdd4
Marc <marc.ferraro1@gmail.com>
2021-03-25 09:26:31 -0500

    Active Storage Migration and avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true
+
+    has_one_attached :avatar
 end
