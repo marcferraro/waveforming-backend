bfe142aa92fbe0ba43e44eeaa6bde7369d57c6e8
Marc <marc.ferraro1@gmail.com>
2021-03-25 13:15:05 -0500

    Create New User

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,4 +5,30 @@ class Api::V1::UsersController < ApplicationController
 
         render json: users
     end
+
+    def create
+
+        user = User.new(new_user_params)
+
+        if user.save
+                #create token and send to front end, save it in localStorage
+
+                payload = {user_id: user.id}
+
+                hmac_secret = "$3CRET"
+
+                token = JWT.encode(payload, hmac_secret, 'HS256')
+
+                render json: {user: UserSerializer.new(user), token: token}
+        else
+            render json: {error: "Unable to create user."}
+            # add more detailed errors later
+        end
+    end
+
+    private
+
+    def new_user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+    end
 end
