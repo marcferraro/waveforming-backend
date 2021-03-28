63325913e9f9a24659a3a165fb3a996081dadee4
Marc <marc.ferraro1@gmail.com>
2021-03-28 18:17:23 -0500

    route added to namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :ooutputs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   namespace :api do 
     namespace :v1 do
       resources :users
+      resources :ooutputs
       
       patch '/avatar/:id', to: 'users#set_avatar'
 
