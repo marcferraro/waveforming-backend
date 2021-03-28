d39ef33c18434f39e086fb0ab994784325b8affe
Marc <marc.ferraro1@gmail.com>
2021-03-28 18:20:49 -0500

    Ooutput controller index

--- app/controllers/ooutputs_controller.rb
+++ app/controllers/ooutputs_controller.rb
@@ -1,2 +1,8 @@
-class OoutputsController < ApplicationController
+class Api::V1::OoutputsController < ApplicationController
+
+    def index
+        ooutputs = Ooutput.all
+
+        render json: ooutputs
+    end
 end
