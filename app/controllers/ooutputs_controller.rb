77221899604daa794bc9d66721eaf8b8c9d41636
Marc <marc.ferraro1@gmail.com>
2021-03-29 13:40:38 -0500

    Ooutputs are saveable!

--- app/controllers/ooutputs_controller.rb
+++ app/controllers/ooutputs_controller.rb
@@ -7,12 +7,20 @@ class Api::V1::OoutputsController < ApplicationController
     end
 
     def create
-        byebug
+        ooutput = Ooutput.new(ooutput_params)
+        ooutput.ooutput_data_uri = params[:ooutput]
+
+        if ooutput.save
+            render json: ooutput
+        else
+            render json: {error: "Unable to save output."}
+        end
+
     end
 
     private
 
     def ooutput_params
-
+        params.permit(:user_id, :title)
     end
 end
