40cab986777e243d64761dc4654c6dcbed00bb1e
Marc <marc.ferraro1@gmail.com>
2021-03-30 10:43:36 -0500

    input_id added to stars

--- db/migrate/20210330144512_create_colors.rb
+++ db/migrate/20210330144512_create_colors.rb
@@ -2,6 +2,7 @@ class CreateColors < ActiveRecord::Migration[6.1]
   def change
     create_table :colors do |t|
       t.string :hex
+      t.integer :input_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_144619) do
 
   create_table "colors", force: :cascade do |t|
     t.string "hex"
+    t.integer "input_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
