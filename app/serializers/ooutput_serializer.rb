abd2a72ad1ecd9c413a403ceefd673b366eb9e2a
Marc <marc.ferraro1@gmail.com>
2021-03-30 15:13:45 -0500

    ooutput serialze user and input

--- app/serializers/ooutput_serializer.rb
+++ app/serializers/ooutput_serializer.rb
@@ -1,3 +1,11 @@
 class OoutputSerializer < ActiveModel::Serializer
-  attributes :id, :ooutput, :user_id, :title, :input_id
+  attributes :id, :ooutput, :user_id, :title, :input_id, :user, :input
+
+  def user
+    object.user
+  end
+
+  def input
+    object.input
+  end
 end
