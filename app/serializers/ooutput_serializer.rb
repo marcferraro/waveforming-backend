f8a42c60a6ebc839c52288434681495cf963fb31
Marc <marc.ferraro1@gmail.com>
2021-04-01 00:21:34 -0500

    serializer fix and pix

--- app/serializers/ooutput_serializer.rb
+++ app/serializers/ooutput_serializer.rb
@@ -1,5 +1,5 @@
 class OoutputSerializer < ActiveModel::Serializer
-  attributes :id, :ooutput, :user_id, :title, :input_id, :user, :input, :stars, :n, :symmetry. :ground, :periodic_input, :periodic_output
+  attributes :id, :ooutput, :user_id, :title, :input_id, :user, :input, :stars, :n, :symmetry, :ground, :periodic_input, :periodic_output
 
   def user
     object.user
