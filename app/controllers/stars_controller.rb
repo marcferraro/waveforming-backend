f9cea0c9ee494694f291c5bc7edfe222a15b3612
Marc <marc.ferraro1@gmail.com>
2021-03-31 21:01:41 -0500

    star actions

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -4,4 +4,22 @@ class StarsController < ApplicationController
 
         render json: stars
     end
+
+    def create
+
+        star = Star.new(star_params)
+
+        if star.save
+            ooutput= Ooutput.find_by(id: params[:ooutput_id])
+            render json: ooutput
+        else
+            render json: {error: "Unable to star."}
+        end
+    end
+
+    private
+
+    def star_params
+        params.permit(:user_id, :ooutput_id)
+    end
 end
