d261a4aac4564e223675f4206a53108b380879d5
Marc <marc.ferraro1@gmail.com>
2021-03-31 21:37:44 -0500

    fixed seeding, added seed asstets folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,10 @@ Input.destroy_all
 Star.destroy_all
 Color.destroy_all
 
-avatar1 = File.open('public/uploads/user/avatar/1/komet.jpeg')
+avatar1 = File.open('db/seed_assets/komet.jpeg')
 user1 = User.new(name: "Marc", username: "marcus", password: "123", password_confirmation: "123", avatar: avatar1)
 user1.save
 
-avatar2 = File.open('public/uploads/user/avatar/2/bear.jpeg')
+avatar2 = File.open('db/seed_assets/bear.png')
 user2 = User.new(name: "Alex", username: "akthegiant", password: "123", password_confirmation: "123", avatar: avatar1)
 user2.save
