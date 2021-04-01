008b46152e7607d5f8f479dfc1993b2291a6d381
Marc <marc.ferraro1@gmail.com>
2021-03-31 21:38:48 -0500

    edited second user

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,5 +17,5 @@ user1 = User.new(name: "Marc", username: "marcus", password: "123", password_con
 user1.save
 
 avatar2 = File.open('db/seed_assets/bear.png')
-user2 = User.new(name: "Alex", username: "akthegiant", password: "123", password_confirmation: "123", avatar: avatar1)
+user2 = User.new(name: "Alex", username: "akthegiant", password: "123", password_confirmation: "123", avatar: avatar2)
 user2.save
