a86d1294a9e30b78e1255e4cc78a82de5de2f55d
Marc <marc.ferraro1@gmail.com>
2021-04-10 16:46:24 -0500

    avatar uploader configured

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,10 +1,11 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage: aws
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
